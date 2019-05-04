e14600ff08ffcb9a18d8e7d29b60edc2cb0c466b
Jason Howlin <jhowlin@me.com>
2019-05-04 08:24:09 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "HD44780LCD",
     dependencies: [
         .package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 from: "1.0.0")
+                 from: "1.1.0")
     ]
 )
